f6ad426c3635c640445b882b86e6a16436c4cc5d
Ruan <ruany@users.noreply.github.com>
2023-06-19 11:55:44 +0200

    Update folder-thumbnailer.sh

--- folder-thumbnailer.sh
+++ folder-thumbnailer.sh
@@ -2,14 +2,12 @@
 
 if [ -f "$2/.folder.jpg" ]; then
 	convert -thumbnail "$1" "$2/.folder.jpg" "$3" 1>/dev/null 2>&1
+elif [ -f "$2/.folder.webp" ]; then
+	convert -thumbnail "$1" "$2/.folder.webp" "$3" 1>/dev/null 2>&1
 elif [ -f "$2/.folder.png" ]; then
 	convert -thumbnail "$1" "$2/.folder.png" "$3" 1>/dev/null 2>&1
-elif [ -f "$2/.folder.svg" ]; then
-	inkscape --export-type=png --export-dpi=500 "$2/.folder.svg" --export-filename="$3" 1>/dev/null 2>&1
-	convert -thumbnail "$1" "$3" "$3" 1>/dev/null 2>&1
 else
-	rm -f "$HOME/.cache/thumbnails/normal/$(echo -n "$4" | md5sum | cut -d " " -f1).png" || \
-	rm -f "$HOME/.thumbnails/normal/$(echo -n "$4" | md5sum | cut -d " " -f1).png" || \
-	rm -f "$HOME/.cache/thumbnails/large/$(echo -n "$4" | md5sum | cut -d " " -f1).png" || \
-	rm -f "$HOME/.thumbnails/large/$(echo -n "$4" | md5sum | cut -d " " -f1).png"
+    file=$(echo -n "$4" | md5sum | cut -d " " -f1).png
+	rm -f "$XDG_CACHE_HOME/thumbnails/normal/$file" || \
+	rm -f "$XDG_CACHE_HOME/thumbnails/large/$file"
 fi
